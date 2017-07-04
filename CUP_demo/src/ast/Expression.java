0bbbaa61594b2fab807bb97e7daf42c5ac83fa7b
Sebastián Torres <Seba_tl@hotmail.com>
2017-07-04 20:32:45 -0300

    Clases ast 2

---
+++ CUP_demo/src/ast/Expression.java
@@ -0,0 +1,13 @@
+package ast;
+
+import java.util.ArrayList;
+
+public class Expression {
+
+	ArrayList<Object> expression;
+	
+	public Expression(ArrayList<Object> expression){
+		this.expression = expression;
+	}
+	
+}
